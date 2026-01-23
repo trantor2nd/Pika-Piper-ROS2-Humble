e69c527a3a87bd5f4e52c1b927af2a0cf130d1ab
trantor2nd <trantor2nd@outlook.com>
2026-01-23 15:51:43 +0800

    tag

--- convert2lerobot.sh
+++ convert2lerobot.sh
@@ -1,7 +1,10 @@
 cd /home/data/Project/Deploy
 python convert2lerobot.py \
     --raw-root /home/data/Dataset/piper_dataset_raw \
-    --output-path /home/data/Dataset/rheovla_dataset_lerobot \
-    --repo-id rheovla_dataset_lerobot \
+    --output-path /home/data/Dataset/dynvla_dataset_lerobot \
+    --repo-id dynvla_dataset_lerobot \
     --fps 10 \
     --overwrite 
+
+#from huggingface_hub import HfApi
+#HfApi().create_tag("trantor2nd/dynvla_dataset_lerobot",tag="v3.0",repo_type="dataset")
